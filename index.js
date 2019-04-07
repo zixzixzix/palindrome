c693f19781be67b5ce6f5b062a3d46f54ce9e4b4
zix <zixzixzix@126.com>
2019-04-07 14:57:37 +0800

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.experts = Phrase;
+module.exports = Phrase;
 
 // Adds `reverse` to all strings.
 String.prototype.reverse = function(){
@@ -10,7 +10,13 @@ function Phrase(content){
   this.content = content;
   // Retruns cotent processed for palindrome testing.
   this.processedContent = function (){
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
+  }
+  //Retruns the latters in the content.
+  this.letters = function (){
+    // const letterRegex = /[a-z]/i;
+    // return Array.from(this.content).filter(c => c.match(letterRegex)).join("");
+    return (this.content.match(/[a-z]/gi) || []).join("");
   }
   // Retruns ture if the phrase is a palindrome, false otherwise.
   this.palindrome = function (){
